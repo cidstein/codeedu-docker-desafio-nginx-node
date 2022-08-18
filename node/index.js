bebf53b9c7c3798cecbe0d5d197feb5c85671f82
¨alcides.souza¨ <alcides.souza@bemobi.com>
2022-08-17 23:32:38 -0300

    fix

--- node/index.js
+++ node/index.js
@@ -1,22 +1,33 @@
 const express = require ('express')
+const mysql = require('mysql')
 const app = express()
 const port = 3000
-// const config = {
-//     host: 'db',
-//     user: 'root',
-//     password: 'root',
-//     database: 'nodedb'
-// }
-// const mysql = require('mysql')
-// const connection = mysql.createConnection(config)
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
 
-// const sql = `INSERT INTO people(name) values('Alcides')`
-// connection.query(sql)
-// connection.end()
-
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
-})
+app.get("/", (req, res) => {
+    const connection = mysql.createConnection(config);
+    const sql = `INSERT INTO people(name) values('Alcides'),('Wesley'),('Luiz')`;
+    connection.query(sql);
+  
+    connection.query(`SELECT * from people`, (err, results) => {
+      if (err) {
+        throw err;
+      }
+      res.send(`<h1>Full Cycle Rocks!</h1>
+                  <ul>
+                    <li>${results[0].name}</li>
+                    <li>${results[1].name}</li>
+                    <li>${results[2].name}</li>
+                  </ul>`);
+    });
+  
+    connection.end();
+  });
 
 app.listen(port, () => {
     console.log('Rodando na porta ' + port)
